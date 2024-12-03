d798b6b84aab443a99f5cf559bdc173df74287e3
Cristian Furtado <csafurtado.dev@gmail.com>
2024-12-02 22:13:08 -0300

    Tentativa 3 (sucesso) para solução do exercício 3123, com vídeo e print do aceite

--- README.md
+++ README.md
@@ -10,15 +10,16 @@
 | 21/1041043 |  Juan Pablo |
 
 ## Sobre 
-Serão realizados 4 exercícios na plataforma no LeetCode que são:
+Serão realizados 3 exercícios na plataforma no LeetCode que são:
 
 - <a href="https://leetcode.com/problems/find-edges-in-shortest-paths/description/">3123. Encontrar arestas nos caminhos mais curtos (Difícil)</a>
 - <a href="https://leetcode.com/problems/keys-and-rooms/description/ ">0841. Chaves e quartos (Médio)</a>
 - <a href="https://leetcode.com/problems/design-graph-with-shortest-path-calculator/description/">2642. Gráfico de design com calculadora de caminho mais curto (Difícil)</a>
-- <a href="https://leetcode.com/problems/minimum-cost-to-make-at-least-one-valid-path-in-a-grid/description/">1368. Custo mínimo para se fazer pelo menos um caminho válido numa grade (Difícil)</a>
 
 ## Screenshots
 ![Aceite do exercício 0841](./imgs/0841_accepted.png "Aceite do exercício 0841")
+![Aceite do exercício 2642](./imgs/2642_accepted.png "Aceite do exercício 2642")
+![Aceite do exercício 3123](./imgs/3123_accepted.png "Aceite do exercício 3123")
 
 ## Instalação 
 **Linguagem**: Python 3.10.*<br>
@@ -32,7 +33,11 @@ Explique como usar seu projeto caso haja algum passo a passo após o comando de 
 Quaisquer outras informações sobre seu projeto podem ser descritas abaixo.
 
 ## Links dos vídeos com explicação
-https://drive.google.com/file/d/100YSPCtVADchO4wVIB1Ao9dB1Pp6NVU-/view?usp=sharing
+[Exercício 2642](https://drive.google.com/file/d/100YSPCtVADchO4wVIB1Ao9dB1Pp6NVU-/view?usp=sharing)
+
+[Exercício 3123](https://drive.google.com/file/d/1F6YFCWdwAX_m_P_W7MjwwwDOLggUxrvD/view?usp=sharing)
+
+[Exercício 0841](https://drive.google.com/file/d/1u_Cgljy3b10rWWWUF-ucp7TpAmaBjVaR/view?usp=sharing)
 
 
 
--- codigos_resolucoes/3123.py
+++ codigos_resolucoes/3123.py
@@ -1,37 +1,26 @@
 import heapq
 
 class Solution:
-    def distances_djikstra(self, n: int, edges: list[list[int]], start):
+    def distances_djikstra(self, n: int, graph: list[list[set]], edges: list[list[int]], start: int):
         """
         Método do algoritmo de Djikstra para encontrar os menores caminhos a partir de um nó inicial
         """
 
-        visited = set()
-        distances = [float('inf') if i != start else 0 for i in range(n)]
+        distances = [float('inf')] * n; distances[start] = 0
         heap_distance = [(distances[start], start)]
 
         while heap_distance:
-            actual_distance, actual_node= heapq.heappop(heap_distance)
+            actual_distance, actual_node = heapq.heappop(heap_distance)
 
-            if actual_node in visited:
-                continue
+            # Essa condição evita lidar com o caminho mais longo da heap (performance)
+            if distances[actual_node] == actual_distance:
+                for neighbor_weight, neighbor_node in graph[actual_node]:
+                
+                    calc_distance = actual_distance + neighbor_weight
 
-            visited.add(actual_node)
-
-            # Verificação para arestas bidirecionais
-            for node_origin, node_dest, weight in edges:
-                if node_origin == actual_node:
-                    neighbor_node, neighbor_weight = node_dest, weight 
-                elif node_dest == actual_node:
-                    neighbor_node, neighbor_weight = node_origin, weight
-                else:
-                    continue
-            
-                calc_distance = actual_distance + neighbor_weight
-
-                if calc_distance < distances[neighbor_node]:
-                    distances[neighbor_node] = calc_distance
-                    heapq.heappush(heap_distance, (calc_distance, neighbor_node))
+                    if calc_distance < distances[neighbor_node]:
+                        distances[neighbor_node] = calc_distance
+                        heapq.heappush(heap_distance, (calc_distance, neighbor_node))
 
         return distances
 
@@ -41,27 +30,35 @@ class Solution:
         de pelo menos 1 dos menores caminhos de se chegar de 0 à n-1. O grafo entrada é não-direcionado.
         """
         
-        distances_from_first = self.distances_djikstra(n, edges, 0)
-        distances_from_last = self.distances_djikstra(n, edges, n-1)
-        total_shortest_path = distances_from_first[n-1]
+        # Cria o grafo com base na lista de arestas como lista de adjacências para facilitar a varredura no Djikstra
+        graph_from_edges = [[] for _ in range(n)]
+        for node_o, node_d, w in edges:
+            graph_from_edges[node_o].append((w, node_d))
+            graph_from_edges[node_d].append((w, node_o))
 
-        print(distances_from_first, distances_from_last)
+        distances_from_first = self.distances_djikstra(n, graph_from_edges, edges, 0)
+        total_shortest_path = distances_from_first[n-1]
+        
+        # Condição que verifica se há um caminho de 0 a n-1
+        if total_shortest_path == float('inf'):
+            return [False] * len(edges)
+        
+        distances_from_last = self.distances_djikstra(n, graph_from_edges, edges, n-1)
 
         result = []
         for node_origin, node_dest, weight in edges:
-            # Verifica se é possível caminho entre os nós '0' e 'n-1' e se a aresta atual faz parte de um dos caminhos mais curtos
-            if total_shortest_path != float('inf') and \
-            (distances_from_first[node_origin] + weight + distances_from_last[node_dest] == total_shortest_path\
+            # Verifica se a aresta atual faz parte de um dos caminhos mais curtos, seja de ida ou de volta (bidirecional)
+            if distances_from_first[node_origin] + weight + distances_from_last[node_dest] == total_shortest_path\
             or \
-            distances_from_first[node_dest] + weight + distances_from_last[node_origin] == total_shortest_path):
+            distances_from_first[node_dest] + weight + distances_from_last[node_origin] == total_shortest_path:
                 result.append(True)
             else:
                 result.append(False)
 
         return result
 
-# edges = [[2,1,6]]
-# vertices = 3
+# edges = [[2,0,1],[0,1,1],[0,3,4],[3,2,2]]
+# vertices = 4
 
 # solucao = Solution()
 
